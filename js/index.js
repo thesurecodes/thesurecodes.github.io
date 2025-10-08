eb974cc1e6986b989f017bd77a8593e10ba899a1
thesurecodes <thesureplugbrand@gmail.com>
2025-10-08 08:38:53 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -129,9 +129,7 @@ window.location.href = mailto;
                     <div class="product-info">
                         <h3 class="product-title">${product.title}</h3>
                         <p class="product-category">${product.category.charAt(0).toUpperCase() + product.category.slice(1)}</p>
-                        <div class="product-price">$${product.price}</div>
-                        <button class="add-to-cart" onclick="addToCart(${product.id})">Add to Cart</button>
-                    </div>
+                       
                 </div>
             `).join('');
         }
